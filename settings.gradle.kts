aff58debcd8d2f4fb773f392673b3c25e2b0f0cd
iamarjun <arjunmanoj1995@gmail.com>
2023-07-30 13:20:20 +0530

    Add feature module following CLEAN architecture

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,7 @@ rootProject.name = "RadiusAgentWeather"
 include(":app")
 include(":core")
 include(":core-ui")
+include(":weather")
+include(":weather:data")
+include(":weather:domain")
+include(":weather:presentation")
