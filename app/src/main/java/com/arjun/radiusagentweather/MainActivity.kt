cfbcd9cca5e8cf988e7ec428f6f443b708923610
iamarjun <arjunmanoj1995@gmail.com>
2023-07-30 12:47:59 +0530

    Upgrade outdated libs

--- app/src/main/java/com/arjun/radiusagentweather/MainActivity.kt
+++ app/src/main/java/com/arjun/radiusagentweather/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            com.arjun.core_ui.theme.RadiusAgentWeatherTheme {
+            RadiusAgentWeatherTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
