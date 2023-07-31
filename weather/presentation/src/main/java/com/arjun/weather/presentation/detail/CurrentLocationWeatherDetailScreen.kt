b1e66d9877706964fef704d4316c0d0622c64bf1
iamarjun <arjunmanoj1995@gmail.com>
2023-07-31 10:01:06 +0530

    Init Weather Detail Screen

--- weather/presentation/src/main/java/com/arjun/weather/presentation/detail/CurrentLocationWeatherDetailScreen.kt
+++ weather/presentation/src/main/java/com/arjun/weather/presentation/detail/CurrentLocationWeatherDetailScreen.kt
@@ -1,10 +1,14 @@
 package com.arjun.weather.presentation.detail
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.annotation.Destination
 
 
 @Composable
 @Destination
-fun CurrentLocationWeatherDetailScreen() {
+fun CurrentLocationWeatherDetailScreen(
+    modifier: Modifier,
+    navigator: CurrentLocationWeatherDetailScreenNavigator
+) {
 }
