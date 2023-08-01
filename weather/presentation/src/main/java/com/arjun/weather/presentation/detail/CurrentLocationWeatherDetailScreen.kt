220d25548171db58e08998ce62c9362370f566a1
iamarjun <arjunmanoj1995@gmail.com>
2023-08-01 20:05:00 +0530

    Implement weekly forecast component

--- weather/presentation/src/main/java/com/arjun/weather/presentation/detail/CurrentLocationWeatherDetailScreen.kt
+++ weather/presentation/src/main/java/com/arjun/weather/presentation/detail/CurrentLocationWeatherDetailScreen.kt
@@ -19,6 +19,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.ArrowUpward
+import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
@@ -40,11 +43,12 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.arjun.core_ui.Dimensions
 import com.arjun.core_ui.LocalSpacing
-import com.arjun.core_ui.theme.RadiusAgentSupportingGray2
+import com.arjun.core_ui.theme.RadiusAgentSupportingGray
 import com.arjun.core_ui.theme.RadiusAgentSupportingGray3
 import com.arjun.weather.presentation.detail.components.UnitText
 import com.ramcosta.composedestinations.annotation.Destination
 import kotlinx.coroutines.flow.collectLatest
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -171,6 +175,135 @@ fun CurrentLocationWeatherDetailScreen(
                     HourlyForecast(modifier, spacing, state)
                 }
 
+                item {
+                    WeeklyForecast(modifier, spacing, state)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun WeeklyForecast(
+    modifier: Modifier,
+    spacing: Dimensions,
+    state: CurrentLocationWeatherDetailScreenContract.State
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(5)
+    ) {
+        Column(
+            modifier = modifier
+                .background(RadiusAgentSupportingGray3)
+                .fillMaxWidth()
+                .padding(spacing.spaceMedium),
+            verticalArrangement = Arrangement.spacedBy(spacing.spaceMedium)
+        ) {
+            Row(
+                modifier = modifier,
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(spacing.spaceMedium)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.CalendarMonth,
+                    contentDescription = "Calendar Icon",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+
+                Text(
+                    text = "5-day Forecast",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            Divider(
+                modifier = modifier.fillMaxWidth(),
+                thickness = 0.5.dp,
+                color = RadiusAgentSupportingGray
+            )
+
+            Column(
+                modifier = modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(spacing.spaceMedium),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                state.currentLocationWeather
+                    ?.forecast
+                    ?.forecastday
+                    ?.filterNotNull()
+                    ?.forEach {
+                        Column(
+                            modifier = modifier,
+                            verticalArrangement = Arrangement.spacedBy(spacing.spaceMedium)
+                        ) {
+                            Row(
+                                modifier = modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                val formatter =
+                                    DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                                val dayOfWeek =
+                                    LocalDate.parse(
+                                        it.date,
+                                        formatter
+                                    ).dayOfWeek
+
+                                Text(
+                                    text = "$dayOfWeek",
+                                    style = MaterialTheme.typography.labelLarge
+                                )
+
+                                Spacer(modifier = modifier.weight(1f))
+
+                                Row(
+                                    modifier = modifier,
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.spacedBy(
+                                        spacing.spaceMedium
+                                    )
+                                ) {
+                                    AsyncImage(
+                                        modifier = modifier.size(20.dp),
+                                        model = "https:${it.day?.condition?.icon}",
+                                        contentDescription = "Current Weather Condition"
+                                    )
+
+                                    Row {
+                                        Icon(
+                                            modifier = modifier,
+                                            imageVector = Icons.Default.ArrowUpward,
+                                            contentDescription = "Max Temperature"
+                                        )
+                                        Text(
+                                            text = "${it.day?.maxtempC}°C",
+                                            style = MaterialTheme.typography.bodyLarge
+                                        )
+                                    }
+
+                                    Row {
+                                        Icon(
+                                            modifier = modifier,
+                                            imageVector = Icons.Default.ArrowDownward,
+                                            contentDescription = "Max Temperature"
+                                        )
+                                        Text(
+                                            text = "${it.day?.mintempC}°C",
+                                            style = MaterialTheme.typography.bodyLarge
+                                        )
+                                    }
+
+                                }
+                            }
+
+                            Divider(
+                                modifier = modifier.fillMaxWidth(),
+                                thickness = 0.25.dp,
+                                color = RadiusAgentSupportingGray
+                            )
+                        }
+                    }
             }
         }
     }
@@ -199,9 +332,9 @@ private fun HourlyForecast(
             )
 
             Divider(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .background(RadiusAgentSupportingGray2)
+                modifier = modifier.fillMaxWidth(),
+                thickness = 0.5.dp,
+                color = RadiusAgentSupportingGray
             )
 
             LazyRow(
